5cf1331341ac81a3b4d4f9ed7411e54ec0f06759
Igor Cerovsky <igor.cerovsky@takeda.com>
2023-08-04 10:11:49 +0200

    dummy testing script

--- README.md
+++ README.md
@@ -15,12 +15,12 @@ Install `latex` from any of the [distributions](https://www.latex-project.org/ge
 `DIR_NAME` is path to directory/folder with Hamilton analysis, e.g. `C:/work/report-gen/reports/230426_AAV9-ELISA_igi_GN004240-033`  
 The working directory **must** contain following files in given format:  
 
-- `*DATE*_*GN*_-_worklist-ELISA.xls`
-- `*DATE*_*GN*_-_*PROTOCOL*_Parameters.csv`
+- `[DATE]_[GN]_-_worklist-ELISA.xls`
+- `[DATE]_[GN]_-_[PROTOCOL]_Parameters.csv`
 
-where `*DATE*` is date format `%y%m%d` (*230801*)  
-`*GN*` is analysis identifier (*GN004240-033*)  
-`*PROTOCOL*` is protocol name (*AAV9-ELISA*)
+where `[DATE]` is date format `%y%m%d` (*230801*)  
+`[GN]` is analysis identifier (*GN004240-033*)  
+`[PROTOCOL]` is protocol name (*AAV9-ELISA*)
 
 Examples:  
 `230426_GN004240-033_-_worklist-ELISA.xls`,  
---
+++ dummy.py
@@ -0,0 +1,28 @@
+from os import path, makedirs
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("workdir", help="working directory of an experiment", default=None)
+    parser.add_argument('--calc', action='store_true', help="use calc files as input", default=False)
+
+    args = parser.parse_args()
+    working_dir = args.workdir
+    calc_input = args.calc
+
+    print(f'Testing scipt :)\nWorking directory is `{working_dir}`, calc input is set to `{calc_input}`\n')
+
+    new_dir = path.join(working_dir, '_foo_dir')
+    makedirs(new_dir, exist_ok=True)
+
+    try:
+        new_file = path.join(new_dir, 'foo.txt')
+        with open(new_file, 'w') as fl:
+            fl.write(f':)\ncalc is {calc_input}\n')
+    except Exception as e:
+        print('Error: ' + str(e))
+
+
+if __name__ == "__main__":
+    main()
