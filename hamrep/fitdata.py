ea6581106bd66c89535b6246a4a9ef61096cc122
Igor Cerovsky <igor.cerovsky@takeda.com>
2023-09-11 14:57:30 +0200

    parameters decription

--- hamrep/fitdata.py
+++ hamrep/fitdata.py
@@ -55,43 +55,70 @@ def inv_func(y, a, b, c, d):
 
 
 def fit_reference(fnc, x, y):
+    """Fit data to reference according to SOP `220726_SOP_Capsid-AAV9-ELISA_V4` `5.1`
+
+    Parameters
+    ----------
+    x : array_like
+        x data
+    y : array_like
+        y data
+    a : float
+
+    Returns
+    -------
+    https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.curve_fit.html
+    """
     p0 = [y.min(), 0.9, x[len(x) - 2], y.max()]
     return curve_fit(fnc, x, y, p0=p0, method='lm', full_output=True, maxfev=10000)
 
 
 def conc_func(x, dil, *popt):
+    """Function computing concentration
+
+    Parameters
+    ----------
+    x : array_like
+        x data
+    dil : float
+        dilution
+    popt : 2D-array
+        fit equation parameters
+
+    Returns
+    -------
+    float64
+        Inverse concentration value
+    """
     return inv_func(x, *popt) * dil
 
 
-def fit_magic(data):
-    popt, pcov, infodict, mesg, ier = fit_reference(func,
-        data['Conc'], data['OD'])
-    
-    print(popt)
-    perr = np.sqrt(np.diag(pcov))
-    print(perr)
-
-    plt.scatter(data['Conc'], data['OD'], marker='+', s=48, linewidths=0.8)
-    plt.xscale('log')
-
-    num_pts = 100
-    t = np.arange(data['Conc'].min(), data['Conc'].max(), (data['Conc'].max() - data['Conc'].min()) / num_pts)
-    plt.plot(t, func(t, *popt), 'r-',
-             label='fit: a=%f, b=%f, c=%f, d=%f' % tuple(popt),
-             linewidth=0.4)
-    plt.xlabel('conc [?cp/ml]')
-    plt.ylabel('OD [?unit]')
-    plt.show()
-
-
 def fit_sheet(popt, pcov, n, confidence_interval=95.0):
+    """Generate dataframe with fit parameters and errors
+
+    Parameters
+    ----------
+    popt : array
+        fit parameters as output from fitting function
+    pcov : 2-D array
+        covariance matrix returned from nonlinear fitting
+    n : int
+        length of data used in fit function used to compute degrees of freedom
+    confidence_interval : float64
+        confidence interval in percent, by default 95%
+
+    Returns
+    -------
+    Pandas dataframe contaning fit parameters and corresponding errors for tabular output
+    """
+
     # `confidence_interval` 95% confidence interval = 100*(1-alpha)
-    alpha = (100.0 - confidence_interval) / 100.0 
-    p = len(popt) # number of parameters
-    dof = max(0, n - p) # number of degrees of freedom
+    alpha = (100.0 - confidence_interval) / 100.0
+    p = len(popt)  # number of parameters
+    dof = max(0, n - p)  # number of degrees of freedom
 
     # student-t value for the dof and confidence level
-    tval = distributions.t.ppf(1.0 - alpha / 2.0, dof) 
+    tval = distributions.t.ppf(1.0 - alpha / 2.0, dof)
 
     sigma_popt = np.empty(len(popt), dtype=np.float64)
     confidence_interval = [None] * 4
@@ -104,20 +131,25 @@ def fit_sheet(popt, pcov, n, confidence_interval=95.0):
     param_names = ['a', 'b', 'c', 'd']
     perr = np.sqrt(np.diag(pcov))
 
-    return pd.DataFrame({'Parameter name': param_names, 'Estimated value':popt,
-        'Error': perr, 'Confidence interval': confidence_interval})
+    return pd.DataFrame({'Parameter name': param_names, 'Estimated value': popt,
+                         'Error': perr, 'Confidence interval': confidence_interval})
 
 
 def backfit(df, param):
     bf = df[['OD_delta', 'plate_layout_conc']].copy()
     bf = bf.reindex(['plate_layout_conc', 'OD_delta'], axis=1)
-    bf.rename(columns={'plate_layout_conc': 'Standard Value [cp/ml]', 'OD_delta': 'Optical density'}, inplace=True)
-    bf.loc[:, ['Concentration backfit [cp/ml]']] = bf.apply(lambda x: inv_func(x['Optical density'], *param), axis=1)
-    bf.loc[:, ['SV to OD fit']] = bf.apply(lambda x: func(x['Standard Value [cp/ml]'], *param), axis=1)
+    bf.rename(columns={
+              'plate_layout_conc': 'Standard Value [cp/ml]', 'OD_delta': 'Optical density'}, inplace=True)
+    bf.loc[:, ['Concentration backfit [cp/ml]']
+           ] = bf.apply(lambda x: inv_func(x['Optical density'], *param), axis=1)
+    bf.loc[:, ['SV to OD fit']] = bf.apply(
+        lambda x: func(x['Standard Value [cp/ml]'], *param), axis=1)
     bf.index.name = 'Well'
-    bf = bf.reindex(['Standard Value [cp/ml]', 'Concentration backfit [cp/ml]', 'Optical density', 'SV to OD fit'], axis=1)
-    bf.loc[:, ['Recovery rate [%]']] = bf.apply(lambda x: (x['Concentration backfit [cp/ml]'] / x['Standard Value [cp/ml]']) * 100.0, axis=1)
-    
+    bf = bf.reindex(['Standard Value [cp/ml]', 'Concentration backfit [cp/ml]',
+                    'Optical density', 'SV to OD fit'], axis=1)
+    bf.loc[:, ['Recovery rate [%]']] = bf.apply(lambda x: (
+        x['Concentration backfit [cp/ml]'] / x['Standard Value [cp/ml]']) * 100.0, axis=1)
+
     return bf
 
 
@@ -130,7 +162,10 @@ def fit_reference_auto_rm(x, y, err_threshold=0.998, verbose=False):
         x-axis values.
     y : array_like
         y-axis value.
-    err_threshold : Eroor threshold to to skip point removal
+    err_threshold : float64
+        error threshold to to skip point removal
+    verbose :  boolean
+        verbose output
 
     Returns
     -------
@@ -140,6 +175,8 @@ def fit_reference_auto_rm(x, y, err_threshold=0.998, verbose=False):
         index of the removed point, -1 if no point is removed
     float64
         error
+    pandas dataframe
+        fit statistics as dataframe
     """
 
     fit_stats = pd.DataFrame(columns=['idx', 'metric', 'note'])
@@ -148,12 +185,13 @@ def fit_reference_auto_rm(x, y, err_threshold=0.998, verbose=False):
         fc = fit_reference(func, x, y)
     except Exception as e:
         estr = '{0}, {1} Reason: {2}'.format(np.nan, -1, str(e))
-        if verbose: print(e)
+        if verbose:
+            print(e)
 
     idx = []
     r2_max = 0.0
     if fc:
-        bfn = lambda l: inv_func(l, *fc[0])
+        def bfn(l): return inv_func(l, *fc[0])
         x_hat = bfn(y)
         try:
             r2_max = r2_score(x, x_hat)
@@ -164,14 +202,15 @@ def fit_reference_auto_rm(x, y, err_threshold=0.998, verbose=False):
         print('R-squared is invalid for nonlinear models!')
         print('metric, index')
         print('{0:.5f}, {1}'.format(r2_max, idx))
-    
-    if r2_max > err_threshold :
+
+    if r2_max > err_threshold:
         fit_stats.loc[len(fit_stats)] = [-1, r2_max, '']
         return fc, idx, r2_max, fit_stats
     elif r2_max == 0.0:
         fit_stats.loc[len(fit_stats)] = [-1, np.nan, '']
     else:
-        fit_stats.loc[len(fit_stats)] = [-1, r2_max, 'metric < threshold ({:.3f} < {:.3f})'.format(r2_max, err_threshold)]
+        fit_stats.loc[len(fit_stats)] = [-1, r2_max,
+                                         'metric < threshold ({:.3f} < {:.3f})'.format(r2_max, err_threshold)]
 
     for i in range(len(x)):
         xd = x.drop([i], axis=0)
@@ -180,24 +219,28 @@ def fit_reference_auto_rm(x, y, err_threshold=0.998, verbose=False):
             fc_i = fit_reference(func, xd.to_numpy(), yd.to_numpy())
         except Exception as e:
             estr = '{0}, {1} Reason: {2}'.format(np.nan, [i], str(e))
-            if verbose: print(e)
+            if verbose:
+                print(e)
             fit_stats.loc[len(fit_stats)] = [i, np.nan, str(e)]
             continue
 
-        bfn = lambda l: inv_func(l, *fc_i[0])
+        def bfn(l): return inv_func(l, *fc_i[0])
         x_hat = bfn(yd)
         try:
             r_squared = r2_score(xd, x_hat)
         except:
-            fit_stats.loc[len(fit_stats)] = [i, r_squared, 'Invalid covariance matrix.']
+            fit_stats.loc[len(fit_stats)] = [i, r_squared,
+                                             'Invalid covariance matrix.']
             continue
 
         if np.isinf(fc_i[1]).any():
-            fit_stats.loc[len(fit_stats)] = [i, r_squared, 'Invalid covariance matrix.']
+            fit_stats.loc[len(fit_stats)] = [i, r_squared,
+                                             'Invalid covariance matrix.']
             continue
 
         fit_stats.loc[len(fit_stats)] = [i, r_squared, '']
-        if verbose: print('{0:.3f}, {1}'.format(r_squared, i))
+        if verbose:
+            print('{0:.3f}, {1}'.format(r_squared, i))
         if (r_squared > r2_max) and not np.isinf(fc_i[1]).any():
             r2_max = r_squared
             fc = fc_i
