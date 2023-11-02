9c48d59a51f7e81021d2087c69c1258e7b8f4369
Igor Cerovsky <igor.cerovsky@takeda.com>
2023-11-02 10:27:09 +0100

    v004

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.3'
+VERSION = '0.0.4'
 DESCRIPTION = 'Hamilton report generation'
 LONG_DESCRIPTION = 'Genrate report from Hamilton potometer output'
 
