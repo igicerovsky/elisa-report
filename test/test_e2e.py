68511391fc18db122d11740b17566a580b95f348
Igor Cerovsky <igor.cerovsky@takeda.com>
2023-12-20 10:47:25 +0100

    unit tests update to reflect change of page break

--- test/test_e2e.py
+++ test/test_e2e.py
@@ -55,20 +55,20 @@ def generic_test(analysis_dir, report_plates_crc, assembly_crc):
 
 def test_e2e_aav9():
     analysis_dir = './test/analysis/230801_AAV9-ELISA_sey_GN004240-053'
-    report_plates_crc = [2535847545, 3489686272, 2582870018]
+    report_plates_crc = [3790190594, 456032002, 1149567454]
     assembly_crc = 2879376160
     generic_test(analysis_dir, report_plates_crc, assembly_crc)
 
 
 def test_e2e_aav9_dilthr():
     analysis_dir = './test/analysis/231122_AAV9-ELISA_sey_GN004240-064'
-    report_plates_crc = [3616146885]
+    report_plates_crc = [3322756084]
     assembly_crc = 51616332
     generic_test(analysis_dir, report_plates_crc, assembly_crc)
 
 
 def test_e2e_aav8():
     analysis_dir = './test/analysis/231024_AAV8-ELISA_sey_GN004240-058'
-    report_plates_crc = [4079393068]
+    report_plates_crc = [693028328]
     assembly_crc = 3592875001
     generic_test(analysis_dir, report_plates_crc, assembly_crc)
